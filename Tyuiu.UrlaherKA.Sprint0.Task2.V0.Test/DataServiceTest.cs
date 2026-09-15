f46fee6a0563144d1f7b2110e9607426b2372896
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-15 23:20:56 +0500

    Task2

---
+++ Tyuiu.UrlaherKA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.UrlaherKA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.UrlaherKA.Sprint0.Task2.V0.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Кирилл";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Кирилл", res);
+        }
+    }
+}
