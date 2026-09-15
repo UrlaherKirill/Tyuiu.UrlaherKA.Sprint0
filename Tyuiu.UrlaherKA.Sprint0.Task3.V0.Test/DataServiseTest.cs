6a3ed6ee75e84801a302854f8eea95f591b50896
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-15 23:40:34 +0500

    Task3

---
+++ Tyuiu.UrlaherKA.Sprint0.Task3.V0.Test/DataServiseTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.UrlaherKA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.UrlaherKA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
